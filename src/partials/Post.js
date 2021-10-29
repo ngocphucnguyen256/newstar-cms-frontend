875f9317fa9d5c743e326dd40b2c2ff56496829c
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-10-29 11:22:06 +0700

    fix auto build autoprefixer

--- src/partials/Post.js
+++ src/partials/Post.js
@@ -65,7 +65,7 @@ const Post =(props)=>{
                   <></>
                 ):(
                   <>
-                    <p className={`${noImg ? "w-0" : " md:w-1/1"}  w-1/1 text-xs sm:text-sm md:text-base`}>
+                    <div className={`${noImg ? "w-0" : " md:w-1/1"}  w-1/1 text-xs sm:text-sm md:text-base`}>
                         {!noImg ? (
                             <Link to={`/post/${data.slug}/${data.id}`} className={`h-1/1 mr-2 mb-1 md:w-2/3 float-left`}>
                                   <img className={` rounded-md transform hover:scale-105 transition duration-700 ease-out ${props.small ? "w-24 sm:w-40 md:w-auto" : "w-52   md:w-auto"}`} src={imgUrl} alt={data.id} />
@@ -76,8 +76,8 @@ const Post =(props)=>{
                         <h3 className=" mb-2 text-sm sm:text-lg lg:text-xl text-bold">
                                 <Link to={`/post/${data.slug}/${data.id}`} className="hover:text-blue-600 transition duration-150 ease-in-out">{data.title}</Link>
                         </h3>
-                        {data.description}
-                  </p>
+                        <p> {data.description}</p>
+                  </div>
                 </>
                 )
               }
